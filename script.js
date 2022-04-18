a3b54db8e9ae1b12280fa09d7bcd9cc97ca263a9
Cade <cjayenn@gmail.com>
2022-04-18 12:32:35 -0400

    working new grid button with prompt checker

--- script.js
+++ script.js
@@ -2,26 +2,7 @@
 
 
 
-
-//grid variables
-const grid = document.querySelector('#grid');
-let gridWidth = 16;
-let gridHeight = 16;
-
-//creating grid from variables gridWidth and gridHeight
-for (let i = 1; i <= gridHeight; i++){
-    const div = document.createElement('div');
-    div.setAttribute('id',`row${i}`);
-    div.setAttribute('class','rows');
-    grid.appendChild(div);
-    for (let j = 1; j <= gridWidth; j++){
-        const row = document.querySelector(`#row${i}`);
-        const div = document.createElement('div');
-        div.setAttribute('class',`column${j} items`);
-        div.setAttribute('id',`item${i}x${j}`);
-        row.appendChild(div);
-    }
-}
+let gridSize = 16;
 
 // squares interact with mouseover event listener
 // checks if background-color is red and turns it white, and vice versa
@@ -33,26 +14,70 @@ function changeColor() {
     }
 }
 
-// mouse hover color change event for grid squares
-const squares = document.querySelectorAll('.items');
-for (let i = 0; i < squares.length; i++){
-    squares[i].addEventListener('mouseover', changeColor);  
+//creating grid from variables gridWidth and gridHeight
+function createGrid(gridSize){
+
+    const grid = document.querySelector('#grid');
+
+    for (let i = 1; i <= gridSize; i++){
+        const div = document.createElement('div');
+        div.setAttribute('id',`row${i}`);
+        div.setAttribute('class','rows');
+        grid.appendChild(div);
+        for (let j = 1; j <= gridSize; j++){
+            const row = document.querySelector(`#row${i}`);
+            const div = document.createElement('div');
+            div.setAttribute('class',`column${j} items`);
+            div.setAttribute('id',`item${i}x${j}`);
+            row.appendChild(div);
+        }
+    }
+    // mouse hover color change event for grid squares
+    const squares = document.querySelectorAll('.items');
+
+    for (let i = 0; i < squares.length; i++){
+        squares[i].addEventListener('mouseover', changeColor);  
+    }
+
+    // clear button event listener
+    // sets all squares background-color to white
+    const clear = document.querySelector('#clear');
+    clear.addEventListener('click',() => {
+        for (let i = 0; i < squares.length; i++){
+            squares[i].style.backgroundColor = 'white';
+        }
+    });
+}
+
+function checkPrompt(text) {
+    
+    if (isNaN(text)) {
+        alert("Not a number!");
+        return 20;
+    } else if (text > 100) {
+        alert("Value too large! Grid size set to 100");
+        return 100;
+    } else{
+        return parseInt(text);
+    }
 }
 
 
-// clear button event listener
-// sets all squares background-color to white
-const clear = document.querySelector('#clear');
-clear.addEventListener('click',() => {
-    for (let i = 0; i < squares.length; i++){
-        squares[i].style.backgroundColor = 'white';
-    }
-});
+// new grid function for event listener
+// prompts user to input tile size, max 100
+// deletes grid
+// creates new grid
+function createNewGrid() {
+    let gridSize = checkPrompt(window.prompt("Input grid size (max: 100)"));
 
+    const grid = document.querySelector('#grid');
+    grid.replaceChildren();
+    createGrid(gridSize);
+}
 
 // new grid button event listener
-// prompts user to input tile size, max 100
 const newGrid = document.querySelector('#new-grid');
-newGrid.addEventListener('click', () => {
-    window.prompt("Input grid size");
-});
+newGrid.addEventListener('click', createNewGrid);
+
+
+createGrid(gridSize);
